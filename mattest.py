9f157b8f1d3cd0a9340225645db8484e63dcd1f1
Joao Nascimento <joaovitor223344joao@gmail.com>
2025-05-02 13:41:34 -0300

    Add files via upload
    Caso o código esteja funcionando deve estar mais ou menos parecido com o gráfico da imagem webp.

---
+++ mattest.py
@@ -0,0 +1,23 @@
+# para baixar o matplotlib use #pip3 install matplotlib direto no terminal
+# importa a biblioteca matplotlib
+import matplotlib.pyplot as plt
+# import5a a 
+import matplotlib.backends.backend_tkagg as tkagg
+
+# Criação do gráfico
+# primeiro array = Y no gráfico, segundo array = X no gráfico
+# obs, o primeiro e segundo array devem ter o a mesma quantidade de valores
+plt.plot([1, 2, 3, 4, 5 , 6, 7, 8], [1, 4, 3, 5, 6, 10, 1, 3])
+
+#Acessando a janela Tkinter
+# pega a atual figura
+fig = plt.gcf()
+# chama o canvas
+canvas = fig.canvas
+
+# Alterando o título da janela usando o canvas
+canvas.manager.window.title("canvas com 8 eixos")
+# Mostra o gráfico
+plt.show()
+
+# para rodar o código dennovo feche a janela do gráfico atual.
